2669d2d8a94143a967ba26789428a51f963adc29
jjoonnoo <118160160+jjoonnoo@users.noreply.github.com>
2022-12-12 10:39:42 +0900

    Update putFile.js

--- static/js/putFile.js
+++ static/js/putFile.js
@@ -1,8 +1,8 @@
 const putFile = file => {
-  const albumBucketName = 'minprojectbuy'; // S3의 버킷 이름
+  const albumBucketName = '비밀'; // S3의 버킷 이름
   const region = 'ap-northeast-2'; // 서울
-  const accessKeyId = 'AKIA5MALC7S46ZH65COI'; // IAM에서 생성한 사용자의 accessKeyId
-  const secretAccessKey = 'VuW8hw/5MX1itDweoT2JwrpRjYQugudC0e/Ii4rd'; // IAM에서 생성한 사용자의 secretAccessKey
+  const accessKeyId = '비밀'; // IAM에서 생성한 사용자의 accessKeyId
+  const secretAccessKey = '비밀'; // IAM에서 생성한 사용자의 secretAccessKey
   const profil_key = file.name
   AWS.config.update({
     region,
